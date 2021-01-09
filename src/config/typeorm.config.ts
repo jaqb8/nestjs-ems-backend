d67443001ee3e29fda0614af7ef3ff420b4368d7
Jakub Aniszewski <qba.anisz@gmail.com>
2021-01-09 23:45:35 +0100

    Add db authentication

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,7 +5,6 @@ const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  url: dbConfig.url,
   useUnifiedTopology: dbConfig.useUnifiedTopology,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: dbConfig.synchronize,
