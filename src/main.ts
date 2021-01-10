3fd98c1ab29bbc396c41efaa831c47d5a4e42ede
Jakub Aniszewski <qba.anisz@gmail.com>
2021-01-10 21:39:36 +0100

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   if (process.env.NODE_ENV === 'development') {
     app.enableCors();
   } else {
-    app.enableCors();
+    app.enableCors({ origin: serverConfig.origin });
   }
 
   const port = process.env.PORT || serverConfig.port;
